0d16b53ee607840150c4123d066998554d4557b6
0xNaN <enzodicicco@gmail.com>
2015-11-18 14:25:15 +0100

    convert two to two

--- src/fizzbuzz.erl
+++ src/fizzbuzz.erl
@@ -1,7 +1,8 @@
 -module(fizzbuzz).
 -export([convert/1]).
 
-convert(1) -> "1".
+convert(1) -> "1";
+convert(2) -> "2".
 
 -ifdef(TEST).
 -include_lib("../test/fizzbuzz.hrl").
--- test/fizzbuzz.hrl
+++ test/fizzbuzz.hrl
@@ -2,3 +2,6 @@
 
 convert_one_to_one_test() ->
     ?assertEqual("1", fizzbuzz:convert(1)).
+
+convert_two_to_two_test() ->
+    ?assertEqual("2", fizzbuzz:convert(2)).
