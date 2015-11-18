70024c5434d4878bc3efc618111efc9fab8c2bb8
0xNaN <enzodicicco@gmail.com>
2015-11-18 14:41:10 +0100

    extracting fizz/buzz

--- src/fizzbuzz.erl
+++ src/fizzbuzz.erl
@@ -1,9 +1,15 @@
 -module(fizzbuzz).
 -export([convert/1]).
 
-convert(N) when N rem 3 =:= 0 -> "Fizz";
-convert(N) when N rem 5 =:= 0 -> "Buzz";
-convert(N) -> integer_to_list(N).
+-define(FIZZ, "Fizz").
+-define(BUZZ, "Buzz").
+
+convert(N) when N rem 3 =:= 0 ->
+    ?FIZZ;
+convert(N) when N rem 5 =:= 0 ->
+    ?BUZZ;
+convert(N) ->
+    integer_to_list(N).
 
 -ifdef(TEST).
 -include_lib("../test/fizzbuzz.hrl").
