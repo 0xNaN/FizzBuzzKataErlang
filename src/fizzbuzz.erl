d2fe1512fb2da8e18edb298cdcae51fc3924a7d1
0xNaN <enzodicicco@gmail.com>
2015-11-18 14:38:52 +0100

    removing duplication

--- src/fizzbuzz.erl
+++ src/fizzbuzz.erl
@@ -1,7 +1,6 @@
 -module(fizzbuzz).
 -export([convert/1]).
 
-convert(5) -> "Buzz";
 convert(N) when N rem 3 =:= 0 -> "Fizz";
 convert(N) when N rem 5 =:= 0 -> "Buzz";
 convert(N) -> integer_to_list(N).
