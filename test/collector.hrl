359f0becde6582c531882a652a06769427f0d211
0xNaN <enzodicicco@gmail.com>
2015-11-18 16:34:35 +0100

    send all received data when filled

--- test/collector.hrl
+++ test/collector.hrl
@@ -9,3 +9,14 @@ receive_one_result_when_space_is_one_test() ->
         {Collector, Ret} ->
             ?assertEqual([{1, "1"}], Ret)
     end.
+
+receive_a_list_of_size_two_result_when_space_is_two_test() ->
+    Receiver = self(),
+    Collector = collector:start(Receiver, 2),
+
+    Collector ! {self(), {1, "1"}},
+    Collector ! {self(), {2, "2"}},
+    receive
+        {Collector, Ret} ->
+            ?assertEqual([{2, "2"}, {1, "1"}], Ret)
+    end.
