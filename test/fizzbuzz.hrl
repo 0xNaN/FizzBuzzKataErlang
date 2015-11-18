c5a034d2a109eaff636b8911a8a082d6c0d06e0e
0xNaN <enzodicicco@gmail.com>
2015-11-18 14:43:35 +0100

    adding a test, unify tests

--- test/fizzbuzz.hrl
+++ test/fizzbuzz.hrl
@@ -6,9 +6,6 @@ convert_one_to_one_test() ->
 convert_two_to_two_test() ->
     ?assertEqual("2", fizzbuzz:convert(2)).
 
-convert_five_to_buzz_test() ->
-    ?assertEqual("Buzz", fizzbuzz:convert(5)).
-
 convert_multiple_of_three_to_fizz_test() ->
     ?assertEqual("Fizz", fizzbuzz:convert(3)),
     ?assertEqual("Fizz", fizzbuzz:convert(6)),
@@ -16,4 +13,6 @@ convert_multiple_of_three_to_fizz_test() ->
     ?assertEqual("Fizz", fizzbuzz:convert(36)).
 
 convert_multiple_of_five_to_buzz_test() ->
-    ?assertEqual("Buzz", fizzbuzz:convert(10)).
+    ?assertEqual("Buzz", fizzbuzz:convert(5)),
+    ?assertEqual("Buzz", fizzbuzz:convert(10)),
+    ?assertEqual("Buzz", fizzbuzz:convert(50)).
